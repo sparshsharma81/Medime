d8f615e7f7a8edb8507a469b6197ac337559207d
sparsh <130464884+sparshsharma81@users.noreply.github.com>
2025-06-08 11:31:00 +0000

    clerk, different buttons, authentication, env file and more+111

---
+++ my-app/app/(auth)/layout.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+const AuthLayout = ({ children }) => {
+    return <div className ="flex justify-center pt-40">{children}</div>
+
+};
+export default AuthLayout;
+
+
+//what every we write inside the layout..it will be applied to both of them 
--- my-app/components/Header.jsx
+++ my-app/components/Header.jsx
@@ -17,7 +17,7 @@ const Header = () =>{
             className="h-10 w-auto object-contain"
           />
             </Link>
-            <div>
+            <div className='flex items-center space-x-2'>
               <SignedOut>
               <SignInButton />
               <Button variant="secondary">Sign In</Button>
