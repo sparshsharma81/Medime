2529b20a873e934277f54b93ad3469abc3d78857
sparsh <130464884+sparshsharma81@users.noreply.github.com>
2025-06-09 11:22:31 +0000

    almost working..but some server error

--- my-app/app/layout.js
+++ my-app/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google"; //this is basically the inner font jo hamne import kiya hai..jise ham use kar rahe hai
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
+import { Toaster } from "sonner";
 import Header from "@/components/Header"; //and this is the header component 
 import { ClerkProvider } from "@clerk/nextjs"; //this is the clerk provider which is for authentication ....
 import { dark } from "@clerk/themes";//this is the dark theme for clerk which is for authentication 
@@ -16,10 +17,11 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return ( 
-     <ClerkProvider appearance={{
-      baseTheme :dark,
-     }}>
-
+      <ClerkProvider
+      appearance={{
+        baseTheme: dark,
+      }}
+    >
       {/* the clerk provide dark themes so that we can use it in the dark theme  */}
       {/* clerk is used basically for authentication --- */}
       {/* basically it will allow only specific users to enter to our application  */}
@@ -29,7 +31,9 @@ export default function RootLayout({ children }) {
 
       {/* ye theme provider basically dark mode k liye hai..shadcn ui se import kiya hai.. */}
       {/* all components are adapting to the dark mode  */}
-
+  <head>
+          <link rel="icon" href="/logo.png" sizes="any" />
+        </head>
       <body className={`${inter.className}`}>
          
           <ThemeProvider
@@ -39,12 +43,11 @@ export default function RootLayout({ children }) {
             disableTransitionOnChange
           >
             {/* yaha pr ham header component add karege  */}
-
+ 
             <Header />
-            <main className="min-h-screen">
-              {/* the min-h-screen dhyan rakhegi ki kam se kam 100 view port height to ho iski  */}
-              {children}
-            </main>
+           <main className="min-h-screen">{children}</main>
+           {/* the min-h-screen dhyan rakhegi ki kam se kam 100 view port height to ho iski  */}
+            <Toaster richColors />
 
             {/* yaha pr ham footer component add karege   */}
             <footer className ="bg-muted/50 py-12">
